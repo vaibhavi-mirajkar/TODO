7230dd16a0b8964970a38730e9166d6205017947
Vaibhavi Mirajkar <vmirajkar931@gmail.com>
2026-01-16 01:39:49 +0530

    updated login/signup pages

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import todo_home,complete_todo,delete_todo,todo_edit 
+from .views import todo_home,complete_todo,delete_todo,todo_edit,signup,user_login
 
 
 urlpatterns = [
-    path('',todo_home,name='todo_home'),
+    path('',user_login,name='login'),
+    path('signup/',signup,name='signup'),
+    path('todo_home/',todo_home,name='todo_home'),
     path('complete/<int:todo_id>/', complete_todo, name='complete_todo'),
     path("delete/<int:todo_id>/", delete_todo, name="delete_todo"),
     path("edit/<int:todo_id>/", todo_edit, name="todo_edit"),
+    
+    
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,redirect
 from .models import TODO
+from django.contrib import messages
+from django.contrib.auth.models import User
+from . import views
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def todo_home(request):
@@ -32,3 +38,53 @@ def todo_edit(request,todo_id):
             todo.save()
             return redirect("todo_home")
     return render(request, 'core/todo_edit.html', {'todo': todo})
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        if not username or not password:
+            messages.error(request,"All fields are required")
+            return redirect("signup")
+        
+        if password != confirm_password:
+            messages.error(request, "passwords do not match")
+            return redirect("signup")
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request,"Username already taken,Please try another one")
+            return redirect("signup")
+        
+        User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+        messages.success(request, "Account created successfully")
+        return redirect("login")
+    
+    return render(request,"core/signup.html")
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if not username or not password:
+            messages.error(request, "Both fields are required")
+            return redirect("login")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("todo_home")
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect("login")
+    
+    return render(request, "core/login.html")
+        
+
